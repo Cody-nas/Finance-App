b0f9737ca88ff06cf7d8f28d5219efecaf50d008
Cody-nas <nasiruadamu214@gmail.com>
2024-11-02 09:58:38 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+// Pre-installed user data
+const userData = {
+  users: [
+    { name: "Victor John", bankName: "UBA", balance: 1000 },
+    { name: "Progress Smith", bankName: "Access Bank", balance: 500 },
+    { name: "David Johnson", bankName: "First Bank", balance: 750 },
+    { name: "Weli Chisom", bankName: "UBA", balance: 1200 },
+  ],
+  currentUserIndex: 0,
+};
+
+function verifyData() {
+  const currentUser = userData.users[userData.currentUserIndex];
+  alert(
+    `Name: ${currentUser.name}\nBank: ${currentUser.bankName}\nBalance: $${currentUser.balance}`
+  );
+}
+
+function sendMoney() {
+  const amountInput = document.getElementById("text-input").value;
+  const amount = parseFloat(amountInput);
+  const currentUser = userData.users[userData.currentUserIndex];
+
+  if (!isNaN(amount) && amount > 0 && amount <= currentUser.balance) {
+    currentUser.balance -= amount;
+    alert(`You have sent $${amount}.`);
+  } else {
+    alert("Invalid amount. Please enter a valid number.");
+  }
+
+  document.getElementById("text-input").value = "";
+}
